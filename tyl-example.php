b5a049a95a81fff4e53aa6ae2895d894836b2598
Matt Stabeler <matt.stabeler@riskbase.uk>
2025-02-17 19:14:47 +0000

    comments and options

--- tyl-example.php
+++ tyl-example.php
@@ -1,25 +1,47 @@
 <?php
 
+  // # Natwest Tyl example
+
+
+  // copy `example.env.php` to `env.php`
+
+  // create an ngrok session
+
+  // `ngrok http 8080`
+
+  // use the external URL as the `BASE_URL` in `env.php`
+
+
+  // Run a local PHP server `php -S localhost:8080` 
+
+  // Open example page in browser
+
+  // `http://localhost:8080/tyl-example.php`
+
+
+
   include("env.php");
 
-  $sharedSecret = getenv("TYL_SECRET");
-  $storename = getenv("TYL_STORE");
-  $baseURL =  getenv("BASE_URL");
+  $sharedSecret = getenv("TYL_SECRET"); 
+  $storename = getenv("TYL_STORE"); // e.g. 7220541890
+  $baseURL =  getenv("BASE_URL"); // e.g. http://localhost:8080
 
   $timezone= "Europe/London";
   $txntype= "sale";
   $checkoutoption = "combinedpage";
   $chargetotal = "13.00";
   $currency = "826";
-  $hash_algorithm = "HMACSHA256";
+  $hash_algorithm = "HMACSHA512";
+  $algo = "SHA512";
   $oid = random_int(100, 999) . "-" .random_int(100, 999);
 
   $txndatetime = getDateTime();
+  // YYYY:MM:DD-hh:mm:ss 
   // $txndatetime = "2025:02:17-07:29:18";
 
 
   $responseFailURL = sprintf("%s/fail.php", $baseURL);
-  $responseSuccessURL = sprintf("%s/succuss", $baseURL);
+  $responseSuccessURL = sprintf("%s/success.php", $baseURL);
   $transactionNotificationURL = sprintf("%s/notify.php", $baseURL);
 
   $separator = "|";
@@ -30,6 +52,7 @@
     "currency"                   => $currency,
     "hash_algorithm"             => $hash_algorithm,
     "oid"                        => $oid,
+    // "merchantTransactionId"     => $oid,
     "responseFailURL"            => $responseFailURL,
     "responseSuccessURL"         => $responseSuccessURL,
     "storename"                  => $storename,
@@ -37,11 +60,27 @@
     "transactionNotificationURL" => $transactionNotificationURL,
     "txndatetime"                => $txndatetime,
     "txntype"                    => $txntype,
+
+    // "bname"          => "John Smith",
+    // "baddr1"         => "123 street",
+    // "baddr2"         => "london",
+    // "bcity"          => "london",
+    // "bstate"         => "london",
+    // "bcountry"         => "GB",
+    // "bzip"         => "EC2M 4AA",
+    // "phone"          => "07790778747",
+    // "email"          => "john.smith@mail.com",
   );
+
+  ksort($fields);
+
+
+
+
 
   $stringToHash = implode($separator, array_values($fields));
   $separator = "|";
-  $hashExtended = createHash($stringToHash, $sharedSecret);
+  $hashExtended = createHash($stringToHash, $sharedSecret, $algo);
 
   $fields["hashExtended"] = $hashExtended;
 
@@ -50,8 +89,8 @@
     return date("Y:m:d-H:i:s");
   }
 
-  function createHash($stringToHash, $sharedSecret) {
-    return base64_encode(hash_hmac('sha256', $stringToHash, $sharedSecret, true));
+  function createHash($stringToHash, $sharedSecret, $algo = "SHA256") {
+    return base64_encode(hash_hmac($algo, $stringToHash, $sharedSecret, true));
   }
 
 ?>
@@ -75,7 +114,11 @@
     <form method="POST" action="https://test.ipg-online.com/connect/gateway/processing">
 
         <?php foreach($fields as $key => $value) { ?>
-        <input type="text" name="<?=$key?>" id=<?=$key?> value="<?=$value?>"><br>
+
+
+        <label>
+          <input type="text" name="<?=$key?>" id=<?=$key?> value="<?=$value?>"> : <?=$key?><br>
+        </label>
 
         <?php }?>
         <br>
